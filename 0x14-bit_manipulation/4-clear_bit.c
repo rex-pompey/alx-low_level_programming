a554ecfcc9eed64ecfebf03a9a80cc97bc65e260
Chiazor Matthew c <mchiazor.chukwuka@gmail.com>
2022-05-30 23:57:14 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,27 +1,17 @@
-/**
- * @file 4-clear_bit.c
- * @author Muhabeid (muhabeid@gmail.com)
- * @brief 
- * @version 0.1
- * @date 2022-04-28
- * 
- * @copyright Copyright (c) 2022
- * 
- */
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * clear_bit - sets the value of a given bit to 0
- * @n: pointer to the number to change
- * @index: index of the bit to clear
- *
- * Return: 1 for success, -1 for failure
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: parameter
+ * @index: index
+ * Return: 1 if works, -1 if error
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > sizeof(n) * 8)
 		return (-1);
-
-	*n = (~(1UL << index) & *n);
+	*n &= ~(1 << index);
 	return (1);
 }
